16d4e26cd6370b9eb0846bb668b7eb7729764f3b
SungJunEun <60536942+SungJunEun@users.noreply.github.com>
2021-07-16 14:05:23 +0000

    finish

---
+++ script.js
@@ -0,0 +1,46 @@
+'use strict';
+function loadItems() {
+  return fetch("data/data.json")
+        .then(response => response.json())
+        .then(data => data.items);
+}
+
+function displayItems(items) {
+  const container = document.querySelector('.items');
+  container.innerHTML = items.map(item => createHTMLstring(item)).join("");  
+}
+function hideItems(items) {
+  
+}
+
+function createHTMLstring(item) {
+  return `<li class="item">
+        <img src=${item.img} alt=${item.type} class="thumbnail-img">
+       ${item.gender} ${item.size} size
+        </li>`
+}
+
+function addEventItems(items) {
+  const logo = document.querySelector('.logo');
+  const menu = document.querySelector('.menu');
+  logo.addEventListener('click', ()=>displayItems(items));
+  menu.addEventListener('click', e => onButtonClick(e, items));
+}
+
+function onButtonClick(e, items) {
+  const key = e.target.dataset.key;
+  const value = e.target.dataset.value;
+  if(key == null  || value == null) {
+    return;
+  }
+  displayItems(items.filter(item => item[key] === value));
+
+  
+}
+
+loadItems()
+  .then(items => {
+    displayItems(items);
+    addEventItems(items);
+  })
+  .catch(console.log);
